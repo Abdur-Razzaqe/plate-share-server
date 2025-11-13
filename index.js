3b0ed9cc7a4f75095386d6c7ba9dcf1afd09d54e
abdurrazzaqe <arazzak714@gmail.com>
2025-11-14 03:13:25 +0600

    requsts collection created

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wnao2sy.mongodb.net/?appName=Cluster0`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,17 +24,24 @@ async function run() {
 
     const db = client.db("share-db");
     const foodCollection = db.collection("foods");
+    const requestsCollection = db.collection("requests");
+    console.log("Connected to bd");
 
     app.get("/foods", async (req, res) => {
-      const result = await foodCollection.find().toArray();
-      res.send(result);
+      const email = req.query.email;
+      const query = email ? { "donator.email": email } : {};
+
+      const result = await foodCollection.find(query).toArray();
+      res.send({ success: true, result });
     });
 
     app.get("/foods/:id", async (req, res) => {
       const { id } = req.params;
-      console.log(id);
-      const objectId = new ObjectId(id);
-      const result = await foodCollection.findOne({ _id: objectId });
+      const result = await foodCollection.findOne({ _id: new ObjectId(id) });
+      if (!result)
+        return res
+          .status(404)
+          .send({ success: false, message: "Food not Found" });
 
       res.send({
         success: true,
@@ -44,68 +51,19 @@ async function run() {
 
     app.post("/foods", async (req, res) => {
       const data = req.body;
-      console.log(data);
-      const result = await foodCollection.insertOne(data);
-      res.send({
-        success: true,
-        result,
-      });
-    });
 
-    // app.get("/foods", async (req, res) => {
-    //   try {
-    //     const { email } = req.query;
-    //     if (!email) {
-    //       return res.status(400).send({
-    //         success: false,
-    //         message: "Donator email is required",
-    //       });
-    //     }
-
-    //     const result = await foodCollection
-    //       .find({ "donator.email": email })
-    //       .toArray();
-
-    //     res.send({
-    //       success: true,
-    //       result,
-    //     });
-    //   } catch (error) {
-    //     console.error(error);
-    //     res.status(500).send({
-    //       success: false,
-    //       message: "Failed to fetch foods",
-    //     });
-    //   }
-    // });
-
-    // app.put("/foods/:id", async (req, res) => {
-    //   const { id } = req.params;
-    //   const updateFood = req.body;
-    //   const objectId = new ObjectId(id);
-    //   const existing = await foodCollection.findOne({ _id: objectId });
-
-    //   if (existing.donator.email !== updateFood.userEmail) {
-    //     return res.status(403).send({
-    //       success: false,
-    //       message: "Unauthorized: You can only update your own foods",
-    //     });
-    //   }
-
-    //   const update = {
-    //     $set: data,
-    //   };
-    //   const result = foodCollection.updateOne({ _id: objectId });
-    //   res.send({
-    //     success: true,
-    //     result,
-    //   });
-    // });
-
-    app.get("/foods", async (req, res) => {
-      const email = req.query.email;
-      const query = email ? { "donator.email": email } : {};
-      const result = await foodCollection.find(query).toArray();
+      const foodData = {
+        ...data,
+        donator: {
+          name: data.donator?.name || data.donator_Name || "Unknown",
+          email:
+            data.donator?.email || data.donator_email || "Unknown@email.com",
+          image: data.donator?.image || data.donator_image || "",
+        },
+        foods_status: data.food_status || "Available",
+        createdAt: new Date(),
+      };
+      const result = await foodCollection.insertOne(foodData);
       res.send({
         success: true,
         result,
@@ -125,8 +83,8 @@ async function run() {
       });
     });
 
-    app.delete("/food/:id", async (req, res) => {
-      const { id } = req.params.id;
+    app.delete("/foods/:id", async (req, res) => {
+      const { id } = req.params;
       const result = await foodCollection.deleteOne({ _id: new ObjectId(id) });
       res.send({
         success: true,
@@ -134,33 +92,43 @@ async function run() {
       });
     });
 
-    app.get("/foods/:id/request", async (req, res) => {
-      const { id } = req.params;
-      const requestData = req.body;
-      const result = await foodCollection.findOne(
-        { _id: new ObjectId(id) },
-        { $push: { requestData } }
-      );
-      res.send(result);
+    app.get("/requests", async (req, res) => {
+      const result = await requestsCollection.find().toArray();
+      res.send({
+        success: true,
+        result,
+      });
     });
 
-    app.get("/foods/:foodId/request/:reqIndex", async (req, res) => {
-      const { foodId, reqIndex } = req.params;
+    app.post("/requests", async (req, res) => {
+      const { id } = req.params;
+      const request = req.body;
+
+      const result = await requestsCollection.insertOne(request);
+      res.send({ success: true, result });
+    });
+
+    app.get("/foodRequest/:foodId", async (req, res) => {
+      const { foodId } = req.params;
+      const requests = await requestsCollection.find({ foodId }).toArray();
+      res.send({ success: true, requests });
+    });
+
+    app.put("/foodRequests/:id", async (req, res) => {
+      const { id } = req.params;
       const { status } = req.body;
-      const food = await foodCollection.findOne({ _id: new ObjectId(foodId) });
-      if (!food || !food.requests) {
-        return res.status(404).send({ message: "Food or request not found" });
-      }
-      food.requests[reqIndex].status = status;
-      if (status === "accepted") {
-        food.food_status = "donated";
-      }
-      const result = await foodCollection.updateOne(
-        { _id: new ObjectId(foodId) },
-        { $set: food }
+      const result = await requestsCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { status } }
       );
 
-      res.send(result);
+      if (status === "accepted") {
+        await requestsCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: { food_status: "donated" } }
+        );
+      }
+      res.send({ success: true, result });
     });
 
     await client.db("admin").command({ ping: 1 });
